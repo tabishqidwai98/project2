3f5c7432b86189b95e2e8803c7d6ae4706be0a1f
Tabish Qidwai <60247314+tabishqidwai98@users.noreply.github.com>
2024-07-20 14:39:48 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+a = [1, 3, 5, 7, 9]
+# similar to forever loop:
+while True:
+    # access by value
+    #for element in a:
+    #    basic.show_number(element)
+    # access by index
+    for index in range(len(a)):
+        basic.show_number(index)
+        basic.show_number(a[i]) # a[i] element in the list a at address i
